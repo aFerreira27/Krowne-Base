108b95d20d1583cc70c1a5ddbaf312afa3f00001
Alex Ferreira <alex.n.ferreira.27@gmail.com>
2025-07-14 04:40:48 +0000

    src/app/layout.tsx (19:7) @ RootLayout
      17 |     <html lang="en" supp

--- src/components/main-layout.tsx
+++ src/components/main-layout.tsx
@@ -25,62 +25,57 @@ const navItems = [
   { href: '/status', label: 'DB Status', icon: BarChart2 },
 ];
 
-function Layout({ children }: { children: React.ReactNode }) {
+export function MainLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
   return (
-    <SidebarProvider>
-      <Sidebar>
-        <SidebarHeader>
-          <div className="flex items-center justify-between p-2">
-             <div className="flex items-center gap-2">
+    <ClientOnly>
+      <SidebarProvider>
+        <Sidebar>
+          <SidebarHeader>
+            <div className="flex items-center justify-between p-2">
+              <div className="flex items-center gap-2">
                 <Image
-                src="https://cdn.ckitchen.com/img/brands/464b22b9-5162-4709-b5a4-c672841b2307/krowne-logo-250618mftv5w.png"
-                alt="Krowne Logo"
-                width={120}
-                height={40}
-                className="w-auto h-8 group-data-[collapsible=icon]:hidden"
+                  src="https://cdn.ckitchen.com/img/brands/464b22b9-5162-4709-b5a4-c672841b2307/krowne-logo-250618mftv5w.png"
+                  alt="Krowne Logo"
+                  width={120}
+                  height={40}
+                  className="w-auto h-8 group-data-[collapsible=icon]:hidden"
                 />
                 <Image
-                src="https://cdn.ckitchen.com/img/brands/464b22b9-5162-4709-b5a4-c672841b2307/krowne-logo-250618mftv5w.png"
-                alt="Krowne Logo"
-                width={40}
-                height={40}
-                className="w-auto h-8 hidden group-data-[collapsible=icon]:block"
+                  src="https://cdn.ckitchen.com/img/brands/464b22b9-5162-4709-b5a4-c672841b2307/krowne-logo-250618mftv5w.png"
+                  alt="Krowne Logo"
+                  width={40}
+                  height={40}
+                  className="w-auto h-8 hidden group-data-[collapsible=icon]:block"
                 />
-             </div>
-            <SidebarTrigger />
-          </div>
-        </SidebarHeader>
-        <SidebarContent>
-          <SidebarMenu>
-            {navItems.map((item) => (
-              <SidebarMenuItem key={item.href}>
-                <SidebarMenuButton
-                  asChild
-                  isActive={pathname === item.href}
-                  tooltip={item.label}
-                >
-                  <Link href={item.href}>
-                    <item.icon />
-                    <span>{item.label}</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            ))}
-          </SidebarMenu>
-        </SidebarContent>
-      </Sidebar>
-      <SidebarInset>
-        <div className="p-4 md:p-6">
-          {children}
-        </div>
-      </SidebarInset>
-    </SidebarProvider>
+              </div>
+              <SidebarTrigger />
+            </div>
+          </SidebarHeader>
+          <SidebarContent>
+            <SidebarMenu>
+              {navItems.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton
+                    asChild
+                    isActive={pathname === item.href}
+                    tooltip={item.label}
+                  >
+                    <Link href={item.href}>
+                      <item.icon />
+                      <span>{item.label}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarContent>
+        </Sidebar>
+        <SidebarInset>
+          <div className="p-4 md:p-6">{children}</div>
+        </SidebarInset>
+      </SidebarProvider>
+    </ClientOnly>
   );
-}
-
-
-export function MainLayout({ children }: { children: React.ReactNode }) {
-  return <ClientOnly><Layout>{children}</Layout></ClientOnly>;
 }
