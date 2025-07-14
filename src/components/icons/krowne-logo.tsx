670b24880b33626b9b8069853654f27df89b1bd5
Alex Ferreira <alex.n.ferreira.27@gmail.com>
2025-07-14 04:29:18 +0000

    it is still wrong, use the krowne img i uploaded

--- src/components/icons/krowne-logo.tsx
+++ src/components/icons/krowne-logo.tsx
@@ -2,12 +2,25 @@ import * as React from 'react';
 
 export const KrowneLogo = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    viewBox="0 0 52 52"
-    fill="currentColor"
     xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
     {...props}
   >
-    <path d="M4.33331 4.33331H19.4999V14.6666H13V47.6666H4.33331V4.33331Z" />
-    <path d="M22.0833 4.33331L38.0833 26L22.0833 47.6666H31.75L47.6666 26L31.75 4.33331H22.0833Z" />
+    <path d="M13 10c3.47-.47 6.36-1.5 8-3.5-2.04 1.7-4.5 3-7.5 3.5" />
+    <path d="M13 14c3.47.47 6.36 1.5 8 3.5-2.04-1.7-4.5-3-7.5-3.5" />
+    <path d="M5.1 4.2C3.1 5.7 2 7.8 2 10c0 2.2 1.1 4.3 3.1 5.8" />
+    <path d="M16 4.2c-2 1.5-3.5 3.7-4.2 6.3" />
+    <path d="M11.8 13.5c.7 2.6 2.2 4.8 4.2 6.3" />
+    <path d="M19.1 12.5c.5-2.5 1-5 1-7.5" />
+    <path d="M19.1 11.5c.5 2.5 1 5 1 7.5" />
+    <path d="M3 10c2.5-.5 5-1.7 7-3.5" />
+    <path d="M3 14c2.5.5 5 1.7 7 3.5" />
   </svg>
 );
